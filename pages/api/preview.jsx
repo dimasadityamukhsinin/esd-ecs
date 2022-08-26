118082c0aace6de028233c6e247f2a9b157952d4
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2022-08-26 16:44:55 +0700

    update

--- pages/api/preview.jsx
+++ pages/api/preview.jsx
@@ -1,4 +1,10 @@
 export default async (req, res) => {
+  // Check the secret and next parameters
+  // This secret should only be known to this API route and the CMS
+  if (req.query.secret !== 'esd-ecs' || !req.query.slug) {
+    return res.status(401).json({ message: 'Invalid token' })
+  }
+
   // Enable Preview Mode by setting the cookies
   res.setPreviewData({})
 
